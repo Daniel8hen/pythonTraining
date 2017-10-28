5316bf62e10ffa43660ded760ad5ec15d0fbaf19
Daniel Hen <Daniel8hen@gmail.com>
2017-10-28 14:25:50 +0300

    after dictionary first study

---
+++ myPythonNotebook.py
@@ -0,0 +1,35 @@
+
+#age = input("Enter your age: ")
+#new_age = float(age) + 50
+#print(new_age)
+#10 / 5
+#x = 2 * 3 ** 2 ** 5
+#print(x)
+#c = "Here"
+#x = c.replace("e","a")
+#print(x)
+#print(c.upper)
+c = "Hi There!"
+#in Python you have two index systems (0...size -1 or -1, -2, etc)
+#print(c[-1]);
+#in Python you can print a part of a string,
+#and the upper bound is always exclusively. e.g.:
+#print(c[0:5]);
+#print(c[-3:-1]);
+#print(c[3:]);
+#Lists - variety of data types (mutable)
+#Remember - item != index
+#l1 = ["Hello", 2, 3];
+#print(l1);
+#print(l1[1]);
+#print(dir(list));
+#l1.append(4);
+#(l1.remove(4));
+#print(l1);
+#Tuples - not mutable - can't be changed - ()
+#t = ("Hello", 3, 4)
+#print(t[-1])
+# print(dir(tuple))
+#Dictionaries - the curly brackets
+d = {"Name":"John", "Surname":"Smith", "Cities": ("Porto", "Tel Aviv", "Madrid")}
+print(d);
