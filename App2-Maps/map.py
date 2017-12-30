274d65f47830c785c94fcdfd8a9a3d7c3e6ee1e3
Daniel Hen <Daniel8hen@gmail.com>
2017-12-30 20:31:57 +0200

    add dynamic lat/lon from .csv

--- App2-Maps/map.py
+++ App2-Maps/map.py
@@ -1,12 +1,18 @@
-# importing the relevant library (folium)
+# importing relevant libraries
 import folium
+import pandas
 # instantiating a map object with 3 different values using folium.Map
-map = folium.Map(location=[32.091612, 34.780355], zoom_start=7, tiles="Mapbox Bright")
+map = folium.Map(location=[38.58, -99.09], zoom_start=4, tiles="Mapbox Bright")
+df = pandas.read_csv("Volcanoes.txt")
+lat = list(df["LAT"])
+lon = list(df["LON"])
 # instantiating featureGroup object which will be added to the map as a coordinate.
 fg = folium.FeatureGroup(name="My map")
-# for loop for iterating over a set of coordinates using .csv file
-for coordinates in [[32.45, 34.78],[32.45, 36.78]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="This is a marker", icon=folium.Icon(color='green')))
+
+# for loop for iterating over two lists - lat, lon, which were derived by a .csv file
+# zip function for running on both indexes in the same iteration
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="This is a marker", icon=folium.Icon(color='green')))
 # adding each of the coordinates to the map
 map.add_child(fg)
 # saving the map
