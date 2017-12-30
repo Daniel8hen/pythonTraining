b0e73414211c3494cc1e6a07f6993e614e280021
Daniel Hen <Daniel8hen@gmail.com>
2017-12-30 19:45:11 +0200

    change in files

--- App2-Maps/map.py
+++ App2-Maps/map.py
@@ -2,7 +2,7 @@ import folium
 map = folium.Map(location=[32.091612, 34.780355], zoom_start=7, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My map")
-for coordinates in [[32.22, 34.78],[39.22, 36.78]]:
+for coordinates in [[32.22, 34.78],[40.22, 36.78]]:
     fg.add_child(folium.Marker(location=coordinates, popup="This is a marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
