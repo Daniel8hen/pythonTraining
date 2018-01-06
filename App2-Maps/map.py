8c60e39aaa47a438812ea4bcc8621b9a5bd4a2f3
Daniel Hen <Daniel8hen@gmail.com>
2018-01-06 15:38:14 +0200

    Ex2. - Final

--- App2-Maps/map.py
+++ App2-Maps/map.py
@@ -16,14 +16,26 @@ lat = list(df["LAT"])
 lon = list(df["LON"])
 elev = list(df["ELEV"])
 # instantiating featureGroup object which will be added to the map as a coordinate.
-fg = folium.FeatureGroup(name="My map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 
 # for loop for iterating over two lists - lat, lon, which were derived by a .csv file
 # zip function for running on both indexes in the same iteration
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=colorProducer(el))))
+    #this is for coordinate point.
+    # fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=colorProducer(el))))
+    #this is for circle point
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill_color=colorProducer(el), fill='true', color='grey', fill_opacity=0.7,  popup=str(el) + " m"))
 # adding each of the coordinates to the map
-map.add_child(fg)
+#create a GeoJson object in order to add a polygon layer
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x ['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 # saving the map
 map.save("Map1.html")
