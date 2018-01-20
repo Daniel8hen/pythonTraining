5bad62895c74a973f389b2d5fd82845334975922
danielhen <daniel8hen@gmail.com>
2018-01-20 17:20:37 +0200

    add DB

---
+++ App5-TkinterGUI/sqlite.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+# Steps to query DB:
+# 1. Connect to DB
+# 2. Create a cursor object
+# 3. Write a SQL query
+# 4. Commit changes to DB
+# 5. Close connection (1)
+def create_table():
+#1
+    conn =sqlite3.connect("lite.db")
+#2
+    cur = conn.cursor()
+#3
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+#4
+    conn.commit()
+#5
+    conn.close()
+
+def insert(item, quantity, price):
+#1
+    conn =sqlite3.connect("lite.db")
+#2
+    cur = conn.cursor()
+#3
+    cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+#4
+    conn.commit()
+#5
+    conn.close()
+
+def view():
+    conn =sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+
+insert("Coffee cup", 10, 5)
+print(view())
