4bec2af3b7bb7b381a8c09c0ca531eeff039ec3f
danielhen <daniel8hen@gmail.com>
2018-02-11 23:01:08 +0200

    add OOP for backend

--- App5-TkinterGUI/App5/backend.py
+++ App5-TkinterGUI/App5/backend.py
@@ -1,51 +1,54 @@
 import sqlite3
 
-def connect():
-    conn =sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
-    conn.commit()
-    conn.close()
+#Session - OOP - now we'll insert everything to a class called Database
+class Database:
 
-def insert(title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
-    conn.commit()
-    conn.close()
+    # the __init__ is a constructor method within the Database
+    def __init__(self, db):
+        conn =sqlite3.connect(db)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+        conn.commit()
+        conn.close()
 
-def view():
-    conn =sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book")
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def insert(self, title, author, year, isbn):
+        conn = sqlite3.connect("books.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
+        conn.commit()
+        conn.close()
 
-# Note - there's a default value ("") for a case where the user would like to seach by 1 value
-def search(title="", author="", year="", isbn=""):
-    conn =sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title =? OR author =? OR year =? OR isbn =?", (title, author, year, isbn))
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def view(self):
+        conn =sqlite3.connect("books.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM book")
+        rows=cur.fetchall()
+        conn.close()
+        return rows
 
-def delete(id):
-    conn =sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM book WHERE id =?", (id,))
-    conn.commit()
-    conn.close()
+    # Note - there's a default value ("") for a case where the user would like to seach by 1 value
+    def search(self, title="", author="", year="", isbn=""):
+        conn =sqlite3.connect("books.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM book WHERE title =? OR author =? OR year =? OR isbn =?", (title, author, year, isbn))
+        rows=cur.fetchall()
+        conn.close()
+        return rows
 
-def update(id, title, author, year, isbn):
-    conn =sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id =?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+    def delete(self, id):
+        conn =sqlite3.connect("books.db")
+        cur = conn.cursor()
+        cur.execute("DELETE FROM book WHERE id =?", (id,))
+        conn.commit()
+        conn.close()
 
-connect()
+    def update(self, id, title, author, year, isbn):
+        conn =sqlite3.connect("books.db")
+        cur = conn.cursor()
+        cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id =?", (title, author, year, isbn, id))
+        conn.commit()
+        conn.close()
+
 # Testing
 # insert("The earth", "John smith", 191, 4123915)
 # delete(3)
--- App5-TkinterGUI/App5/frontend.py
+++ App5-TkinterGUI/App5/frontend.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import backend
+from backend import Database
+
+database = Database("books.db")
 
 def get_selected_row(event):
     try:
@@ -18,24 +20,24 @@ def get_selected_row(event):
         pass
 def view_command():
     list1.delete(0, END)
-    for row in backend.view():
+    for row in database.view():
         list1.insert(END, row)
 
 def search_command():
     list1.delete(0, END)
-    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+    for row in database.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
         list1.insert(END, row)
 
 def insert_command():
-    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    database.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
     list1.delete(0, END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
 def delete_command():
-    backend.delete(selected_tuple[0])
+    database.delete(selected_tuple[0])
 
 def update_command():
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    database.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
 
 # instantiate a window object
 window=Tk()
