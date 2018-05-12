65075543349c5c4ba19d4ca218f71b61d2b44c78
Daniel Hen <daniel8hen@gmail.com>
2018-05-12 23:21:20 +0300

    App after created DB

--- Apps/App9-DataCollector_WebApp/app.py
+++ Apps/App9-DataCollector_WebApp/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost/height_collector'
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_=db.Column(db.String(120), unique=True)
+    height_=db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
 
 @app.route("/")
 def index():
