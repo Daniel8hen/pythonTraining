34944dd6cee52e84ea23a58e08b51e6c15b66cd7
Daniel8hen <daniel8hen@gmail.com>
2018-06-05 21:41:06 +0300

    start numpy lecture

---
+++ OngoingStudy/pythonForDS/NumPy/numpyLectures.py
@@ -0,0 +1,21 @@
+# numpy array - 1D / 2D
+# casting a numpy array from python object
+# my_list = [1,2,3]
+import numpy as np
+# arr = np.array(my_list)
+# my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# print(np.array(my_matrix)) --> #2 dimensional array
+# arr = np.arange(0, 11, 5) #start, #end (excluding), #step - for quickly generating array using numpy
+#print(arr)
+#print(np.zeros((6,6))) #pass 1 value / tuple(!)
+#print(np.ones(1))
+#print(np.linspace(0,5,100)) #generating an array with "number of points" (3rd element)
+#print(np.eye(5))
+
+# print(np.random.rand(5,5)) - #no need for tuple!
+# print(np.random.randn(2)) - #from standart distribution
+#np.random.randint(1,100,10) - #10 random numbers between 1 and 99
+# arr.reshape(5,5) - #reshape existing array
+#argmax for index of max, max of max value, same for min...
+#from numpy.random import randint
+# print(randint(2,10))
