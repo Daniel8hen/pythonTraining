ed9a82d12486d9e6e38849d11ce4f31e04452e42
Daniel8hen <daniel8hen@gmail.com>
2018-06-30 15:01:35 +0300

    changes for matplotlib

---
+++ pythonForDS/Matplotlib/lecture.py
@@ -0,0 +1,10 @@
+# most popular plotting lib for Python
+# gives control over every aspect of figure
+#matplotlib.org - official website with examples + code examples
+import matplotlib.pyplot as plt
+# ***plt.show() will be used each time we'd like to show the plot
+import numpy as np
+x = np.linspace(0,5,11)
+y = x ** 2
+print(x)
+print(y)
