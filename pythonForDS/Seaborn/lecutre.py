8fcb794b511423f1998f83a70f112f219da6cca9
Daniel8hen <daniel8hen@gmail.com>
2018-07-21 18:04:05 +0300

    change lecture

--- pythonForDS/Seaborn/lecutre.py
+++ pythonForDS/Seaborn/lecutre.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt # because not using jupyter notebook
 import numpy as np
 # if using jupyter notebook use %matplotlib inline for visualization inline
 tips = sns.load_dataset('tips')
-print(tips.head())
+# print(tips.head())
 # print(tips.head()) #people who had a meal and left tip in the end
 # sns.distplot(tips['total_bill'],kde=False,bins=40) #removing the line above the distributor graph
 # sns.jointplot(x='total_bill',y='tip',data=tips)
@@ -30,17 +30,41 @@ print(tips.head())
 # sns.swarmplot(x='day',y='total_bill',data=tips,color='black')
 # sns.factorplot(x='day',y='total_bill',data=tips,kind='bar') - available for specific graphs usage
 # matrix plots
-flights=sns.load_dataset('flights')
+# flights=sns.load_dataset('flights')
 # print(flights.head())
 
-tc = tips.corr()
+# tc = tips.corr()
 # print(tips.corr())
 # sns.heatmap(tc, annot=True, cmap='coolwarm') #can use annot for annotation, cmap for colormap
 
 #another matrix manipulation
-fp = flights.pivot_table(index='month',columns='year',values='passengers')
+# fp = flights.pivot_table(index='month',columns='year',values='passengers')
 # sns.heatmap(fp,cmap='coolwarm',linecolor='black',linewidths=1) #and tons of options to customize
-sns.clustermap(fp,cmap='coolwarm',standard_scale=1) #clustering on the data
-plt.show()
-
+# sns.clustermap(fp,cmap='coolwarm',standard_scale=1) #clustering on the data
+# iris = sns.load_dataset('iris')
+# print(iris.head())
+# print(iris['species'].unique())
 #Grids
+# g = sns.FacetGrid(data=tips,col='time',row='smoker') # facet grid for the tips dataset
+# g = sns.PairGrid(iris) # makes all values and gives you control for subplots
+# btw, pairplot is a simplified version of pairgrid
+# g.map(plt.scatter)
+# g.map_diag(sns.distplot)
+# g.map_upper(plt.scatter)
+# g.map_lower(sns.kdeplot)
+# g.map(sns.distplot,'total_bill')
+# g.map(plt.scatter,'total_bill','tip')
+
+#Regression Plots - lmplot for linear regression plotting
+#sns.lmplot(x='total_bill',y='tip',data=tips,hue='sex',markers=['o','v'],
+# scatter_kws={'s':100}) #x axis vs y axis
+# sns.lmplot(x='total_bill',y='tip',data=tips,col='day',hue='sex', aspect=0.6,size=8) #can add row as well
+# style and color session
+sns.set_style('whitegrid') #background style
+# sns.countplot(x='sex',data=tips)
+#remove spines by
+# sns.despine() #can be used only on top, left, bottom, right as well
+# plt.figure(figsize=(12,3))
+# sns.set_context('poster',font_scale=5)
+sns.lmplot(x='total_bill',y='tip',data=tips,hue='sex',palette='seismic')
+plt.show()
