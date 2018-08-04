234dd12823b7d53e28ab3587b2b8f2f40cb4950e
Daniel8hen <daniel8hen@gmail.com>
2018-08-04 17:49:40 +0300

    add lecture notebook + notes

---
+++ pythonForDS/geo-visualization/lecture.py
@@ -0,0 +1,4 @@
+#notes here are from Choropleth Maps lecture
+import plotly.plotly as py
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
