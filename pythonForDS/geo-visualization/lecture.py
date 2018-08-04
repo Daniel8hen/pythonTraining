8a2452e483587caf9b162ce39382877f0a9c36d0
Daniel8hen <daniel8hen@gmail.com>
2018-08-04 17:50:20 +0300

    remove lecture.py since using notebook

--- pythonForDS/geo-visualization/lecture.py
+++
@@ -1,4 +0,0 @@
-#notes here are from Choropleth Maps lecture
-import plotly.plotly as py
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-init_notebook_mode(connected=True)
